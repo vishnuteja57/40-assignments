fcdf118d9724bc3246b408ac1554708d895b8920
vishnuteja57 <82230760+vishnuteja57@users.noreply.github.com>
2022-03-15 23:29:58 +0530

    Delete Capstone directory

--- Capstone/FoodBox backend/src/main/java/com/FoodBox/FoodBox/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.FoodBox.FoodBox.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.FoodBox.FoodBox.model.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Integer>{
-
-}
